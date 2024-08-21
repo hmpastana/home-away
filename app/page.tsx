01300ae73dbd01aeaaa9950c7fd74c3195dff264
Hellen Marques Pasrana <hellenmpastana@gmail.com>
2024-08-21 12:07:47 -0600

    Update list of categories and properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
-import { Button } from "@/components/ui/button"
-// import { ChevronRight } from "lucide-react"
+import LoadingCards from '@/components/card/LoadingCards';
+import CategoriesList from '@/components/home/CategoriesList';
+import PropertiesContainer from '@/components/home/PropertiesContainer';
+import { Suspense } from 'react';
 
-export default function HomePage() {
+function HomePage({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
   return (
-    <div>
-      <h1 className='text-3xl'>HomePage</h1>
-      <Button variant="secondary">click me</Button>
-
-    </div>
-  )
+    <section>
+      <CategoriesList
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams?.category}
+          search={searchParams?.search}
+        />
+      </Suspense>
+    </section>
+  );
 }
+export default HomePage;
