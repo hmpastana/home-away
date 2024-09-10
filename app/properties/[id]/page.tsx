a64edf3d366a99ed53d13221af421ea4ad54f7d2
Hellen Marques Pasrana <hellenmpastana@gmail.com>
2024-09-10 11:46:33 -0600

    create amenities and description properties components

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -7,12 +7,21 @@ import ImageContainer from '../ImageContainer';
 import PropertyRating from '@/components/card/PropertyRating';
 import BookingCalendar from '../booking/BookingCalendar';
 import PropertyDetails from '../PropertyDetails';
+import UserInfo from '../UserInfo';
+import Amenities from '../Amenities';
+import Description from '../Description';
+import { Separator } from '@/components/ui/separator';
+
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
     const property = await fetchPropertyDetails(params.id);
     if (!property) redirect('/');
     const { baths, bedrooms, beds, guests } = property;
     const details = { baths, bedrooms, beds, guests };
+
+    const firstName = property.profile.firstName;
+    const profileImage = property.profile.profileImage;
+
     return <section>
         <BreadCrumbs name={property.name} />
         <header className='flex justify-between items-center mt-4'>
@@ -30,12 +39,17 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
                     <PropertyRating inPage propertyId={property.id} />
                 </div>
                 <PropertyDetails details={details} />
+                <UserInfo profile={{ firstName, profileImage }} />
+                <Separator className='mt-4' />
+                <Description description={property.description} />
+                <Amenities amenities={property.amenities} />
             </div>
             <div className='lg:col-span-4 flex flex-col items-center'>
                 {/* calendar */}
                 <BookingCalendar />
             </div>
         </section>
+
     </section>
 }
 export default PropertyDetailsPage;
